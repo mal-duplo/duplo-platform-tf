66671df648c07c83abd3638282e5e0be877d227f
Malaquias Marconi <malaquias@duplocloud.net>
2025-11-11 17:00:34 -0600

    added tenant b to stacks and cicd

---
+++ stacks/tenants/tenant2/tenant.tf
@@ -0,0 +1,25 @@
+resource "duplocloud_tenant" "this" {
+  account_name   = local.name
+  plan_id        = local.infra_name
+  allow_deletion = (lower(lookup(coalesce(var.settings, {}), "delete_protection", "false")) == "false")
+
+  lifecycle {
+    ignore_changes = [
+      account_name,
+      plan_id
+    ]
+  }
+}
+
+resource "duplocloud_tenant_config" "this" {
+  count     = var.settings != null ? 1 : 0
+  tenant_id = local.tenant_id
+
+  dynamic "setting" {
+    for_each = var.settings
+    content {
+      key   = setting.key
+      value = setting.value
+    }
+  }
+}
