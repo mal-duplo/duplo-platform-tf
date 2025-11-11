cb00c2032747ec6dc83ddaff86ab543773fdecfa
Malaquias Marconi <malaquias@duplocloud.net>
2025-11-11 16:38:29 -0600

    updated locals in tenants stack

--- stacks/tenants/tenant1/main.tf
+++ stacks/tenants/tenant1/main.tf
@@ -1,11 +1,18 @@
 locals {
   name      = coalesce(var.name, terraform.workspace)
-  # match the module’s logic: allow override via var.infra_name, else use infra state
-  infra_name = coalesce(var.infra_name, data.terraform_remote_state.infra.outputs.infra_name)
 
-  # only needed if you enable `parent`, `grants`, or `security_rules`
-  tenant_id  = duplocloud_tenant.this.tenant_id
-  parent_obj = var.parent != null ? data.duplocloud_tenant.parent[0] : null
+  # Resolve infra name:
+  # 1) prefer explicit var.infra_name (from tenant.tfvars)
+  # 2) if a parent tenant is specified, inherit its plan_id
+  # 3) otherwise default to "default"
+  infra_name = coalesce(
+    var.infra_name,
+    var.parent != null ? local.parent.plan_id : "default"
+  )
+
+  # Only needed if you enable `parent`, `grants`, or `security_rules`
+  tenant_id = duplocloud_tenant.this.tenant_id
+  parent    = var.parent != null ? data.duplocloud_tenant.parent[0] : null
 
   siblings = [
     for grant in var.grants : grant.grantee
